67bec1c0adb61e6dfab258d7ce4e9aa3096a72f1
Stephen Larson <stephen.larson@gmail.com>
2016-05-19 09:54:28 -0400

    added content for nxserver setup and start

--- startup.sh
+++ startup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+###BEGIN NX STUFF
+cp /mnt/passwd /etc/passwd
+cp /mnt/group /etc/group
+cp /mnt/shadow /etc/shadow
+rm /var/lib/nxserver/home/.ssh/authorized_keys2
+/usr/lib/nx/nxsetup --install --clean --purge --auto --setup-nomachine-key
 
+mkdir /var/run/sshd
+#/usr/bin/supervisord -c /supervisord.conf
+
+mkdir /tmp/.X11-unix
+mkdir /tmp/.ICE-unix
+chmod 1777 /tmp/.X11-unix 
+/etc/init.d/freenx-server start
+###END NX STUFF
+
+###START VNC STUFF
 # create an ubuntu user
 # PASS=`pwgen -c -n -1 10`
 PASS=ubuntu
