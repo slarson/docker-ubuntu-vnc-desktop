8efc7a026a8612906d1514b46a7872275d30f691
Stephen Larson <stephen.larson@gmail.com>
2016-05-20 16:03:39 -0400

    small fix

--- run_eclipse.sh
+++ run_eclipse.sh
@@ -2,4 +2,6 @@
 
 xhost +
 
-sudo docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`/.eclipse-docker:/home/developer -v `pwd`:/workspace slarson/docker-eclipse
+cd Dropbox/cloud-workspace
+
+sudo docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`/.eclipse-docker:/home/developer -v `pwd`:/workspace slarson/docker-eclipse &
